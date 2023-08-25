3ec8393765f3385bab2c37a0aacf099befc4bea3
Corey Shaw <coshaw@adobe.com>
2023-08-24 22:01:36 -0600

    Enforce elevation to admin if necessary

--- configure-wsl-networking.ps1
+++ configure-wsl-networking.ps1
@@ -11,6 +11,20 @@ $state_file = "$HOME\wsl-added-routes.txt"
 
 ########### End Configuration Parameters
 
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
 echo "===================="
 echo "= WSL2 VPN Support ="
 echo "===================="
